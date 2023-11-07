b710081be0a9c84865c62dc39fb20c7a9943c84b
Hoang Le <hoanglh7@hdbank.com.vn>
2023-11-07 23:52:29 +0700

    s

--- db/main.go
+++ db/main.go
@@ -3,6 +3,7 @@ package db
 import (
 	"bee-go-demo/config"
 	"bee-go-demo/model"
+	"fmt"
 
 	"github.com/beego/beego/v2/client/orm"
 	_ "github.com/lib/pq"
@@ -11,7 +12,7 @@ import (
 func Init(cfg *config.Configuration) {
 	// set default database
 	orm.RegisterDriver("postgres", orm.DRPostgres)
-
+	fmt.Println("DbDsn", cfg.DbDsn)
 	// set default database
 	orm.RegisterDataBase("default", "postgres", cfg.DbDsn)
 
